37e77d223f13f064138e80c4678b91e449311a48
Yashdeep Tandon <yashdeeptandon007@gmail.com>
2025-03-23 19:43:50 +0530

    client side fix

--- client/src/components/about-me/about-me.tsx
+++ client/src/components/about-me/about-me.tsx
@@ -1,26 +1,16 @@
-import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
-
 import { Particles } from "@/components/magicui/particles";
 
 export function AboutMe() {
-  const { resolvedTheme } = useTheme();
-  const [color, setColor] = useState("#ffffff");
-
-  useEffect(() => {
-    setColor(resolvedTheme === "dark" ? "#ffffff" : "#000000");
-  }, [resolvedTheme]);
-
   return (
-    <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg bg-background">
-      <span className="pointer-events-none z-10 whitespace-pre-wrap text-center text-8xl font-semibold leading-none font-code">
+    <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden rounded-lg">
+      <span className="pointer-events-none z-10 whitespace-pre-wrap text-center text-8xl font-semibold leading-none font-code text-red-600">
         Yashdeep Tandon
       </span>
       <Particles
         className="absolute inset-0 z-0"
         quantity={100}
         ease={80}
-        color={color}
+        color="#ffffff"
         refresh
       />
     </div>
