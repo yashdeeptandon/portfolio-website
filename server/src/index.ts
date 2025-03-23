ea79d42889cffdfaa2ebb2a7c7027413ebb154cb
Yashdeep Tandon <yashdeeptandon007@gmail.com>
2025-03-23 15:10:29 +0530

    server basic setup

---
+++ server/src/index.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (_, res) => {
+  res.send("Portfolio API is running with TypeScript!");
+});
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGO_URI as string)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => console.error(err));
